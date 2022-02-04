6df9a4437bb4a7e0a6f62ea2b7d352e874f4209c
Bünyamin Keleş <57811279+bunyaminkeles@users.noreply.github.com>
2022-02-04 19:47:03 +0300

    forto_22

---
+++ _Projects/OtherWorks/01-BasicAlgorithms/password_reminder.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#Password Reminder
+
+user = str(input())
+name = "Joseph" 
+if name == user:
+  print("Hello,{}! Password: w @ 12 " .format(user))
+else :
+  print("Hello,{}! See you later." .format(user))
+
